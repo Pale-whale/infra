fe749b561996a5d352406c2265fd6b2170cc3dc5
Pale-whale <pierre.p@padoa-group.com>
2025-11-04 22:16:41 +0100

    feat: allow argocd to run on uninitialized nodes since he will install controller

--- kubernetes_terraform/argocd.tf
+++ kubernetes_terraform/argocd.tf
@@ -27,6 +27,21 @@ resource "helm_release" "argocd_bootstrap" {
   chart            = "argo-cd"
   version          = var.argocd_version
   create_namespace = true
+
+  set = [
+    {
+      name = "global.tolerations[0].key"
+      value = "node.cloudprovider.kubernetes.io/uninitialized"
+    },
+    {
+      name = "global.tolerations[0].operator"
+      value = "Exists"
+    },
+    {
+      name = "global.tolerations[0].effect"
+      value = "NoSchedule"
+    },
+  ]
 }
 
 resource "helm_release" "argocd_extra_objects" {
@@ -40,6 +55,21 @@ resource "helm_release" "argocd_extra_objects" {
   version          = var.argocd_version
   create_namespace = true
   upgrade_install  = true
+
+  set = [
+    {
+      name = "global.tolerations[0].key"
+      value = "node.cloudprovider.kubernetes.io/uninitialized"
+    },
+    {
+      name = "global.tolerations[0].operator"
+      value = "Exists"
+    },
+    {
+      name = "global.tolerations[0].effect"
+      value = "NoSchedule"
+    },
+  ]
 
   values = [templatefile("${path.module}/values/argocd.yaml.tpl", {
     extra_applications = var.argocd_extra_applications
