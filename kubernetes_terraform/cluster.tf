593a2432eadcb497e71e058df9a808fb2d0bbc99
Pale-whale <pierre.p@padoa-group.com>
2025-11-05 00:35:55 +0100

    fix: copypasta

--- kubernetes_terraform/cluster.tf
+++ kubernetes_terraform/cluster.tf
@@ -98,7 +98,7 @@ resource "talos_machine_configuration_apply" "worker" {
       network_ip_prefix  = "24"
       network_gateway    = var.default_gateway
       proxmox_datacenter = var.cluster_name
-      vm_id              = local.controlplane[each.key].vm_id
+      vm_id              = local.workers[each.key].vm_id
     })
   ]
 }
